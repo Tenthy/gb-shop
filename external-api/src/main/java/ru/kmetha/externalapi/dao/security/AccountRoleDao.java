40d28da514a8f32e34d693ddbaf830acde40f2c7
Kmetha <rakursdead@gmail.com>
2022-06-23 18:02:55 +0300

    leson 5

---
+++ external-api/src/main/java/ru/kmetha/externalapi/dao/security/AccountRoleDao.java
@@ -0,0 +1,8 @@
+package ru.kmetha.externalapi.dao.security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.kmetha.externalapi.entity.security.AccountRole;
+
+public interface AccountRoleDao extends JpaRepository<AccountRole, Long> {
+
+}

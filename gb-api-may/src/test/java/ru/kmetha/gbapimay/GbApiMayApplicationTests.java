b67605263ce707242f4f7eb965a3d6f4ce6a443e
Kmetha <rakursdead@gmail.com>
2022-06-23 18:08:16 +0300

    Commit: Add 'gb-api-may'

---
+++ gb-api-may/src/test/java/ru/kmetha/gbapimay/GbApiMayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kmetha.gbapimay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbApiMayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
